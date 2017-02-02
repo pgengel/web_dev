e2b0459f6ec19a5b6c17d1279cd083493a67b168
Pieter Engelbrecht <pieter.engelbrecht@derivco.co.za>
2017-02-02 12:31:29 +0200

    Getting the input values from the input fields.

--- Budgety/app.js
+++ Budgety/app.js
@@ -6,28 +6,52 @@ var budegetController = (function() {
 
     return {
 
-    }
+    };
 
 })();
 
 //UI CONTROLLER
 var UIController = (function() {
-    //
+    
+    var DOMStrings = {
+        inputType : '.add__type',
+        inputDescription : '.add__description',
+        inputValue : '.add__value',
+        inputBtn : '.add__btn',
+    };
 
-    return{
+    return {
 
-    }
+        // get the inputs from the UI
+        getInput: function() {                 
+            return {
+                //return an object with these 3 things
+                type : document.querySelector(DOMStrings.inputType).value,// Will be either inc or exp
+                desciption : document.querySelector(DOMStrings.inputDescription).value,
+                value : document.querySelector(DOMStrings.inputValue).value,
+            };
+        },
+
+        getDOMStrings : function() {
+            return DOMStrings;
+        },
+
+    };
 
 })();
 
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
+    var DOM = UICtrl.getDOMStrings();
+
     var ctrlAddItem = function() {
         
         console.log("Enter has been pressed.");
 
-        //1. Get the field input data
+        //1. Get the field input data from the UI.
+        var input = UICtrl.getInput();
+        console.log(input);
 
         //2. Add the item to the budget controller.
 
@@ -41,7 +65,7 @@ var controller = (function(budgetCtrl, UICtrl) {
     }
 
     //event listiner
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
     //the key press happens on the global event not just on the click.
     document.addEventListener('keypress', function(event) {
