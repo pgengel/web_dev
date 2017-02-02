a83a616b707ddbd7d9d0786451878464cf94f916
Pieter Engelbrecht <pieter.engelbrecht@derivco.co.za>
2017-02-02 16:47:55 +0200

    Clearing the input fields.

--- Budgety/app.js
+++ Budgety/app.js
@@ -114,6 +114,17 @@ var UIController = (function() {
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
         },
 
+        clearFields: function() {
+            var fields, fieldsArr;
+            fields = document.querySelectorAll(DOMStrings.inputDescription +', ' + DOMStrings.inputValue);
+
+            fieldsArr = Array.prototype.slice.call(fields);   
+
+            fieldsArr.forEach(function(curr, index, array) {
+                curr.value = "";    
+            });
+        },
+
         getDOMStrings : function() {
             return DOMStrings;
         },
@@ -156,9 +167,11 @@ var controller = (function(budgetCtrl, UICtrl) {
         //3. Add the item to the UI
         UICtrl.addListItem(newItem, input.type);
 
-        //4. Calculate the budget
+        //4. Clear the fields
+        UICtrl.clearFields();
+        //5. Calculate the budget
 
-        //5. Display the budget on the UI.
+        //6. Display the budget on the UI.
 
 
     }
