b87168aa5b5f994fd3715cf0524e08ec4bd6e505
Pieter Engelbrecht <pieter.engelbrecht@derivco.co.za>
2017-02-02 14:16:06 +0200

    added the new item.

--- Budgety/app.js
+++ Budgety/app.js
@@ -2,10 +2,65 @@
 
 //BUDGET CONTROLLER
 var budegetController = (function() {
-    
+
+    // functions constrcutor.
+    var Expense = function(id, desciption, value) {
+        this.id = id;
+        this.desciption = desciption;
+        this.value = value;
+    };     
+
+    // functions constrcutor.
+    var Income = function(id, desciption, value) {
+        this.id = id;
+        this.desciption = desciption;
+        this.value = value;
+    };   
+
+    var data = {
+        allitems : {
+            exp : [],  
+            inc : [], 
+        },
+        totals : {
+            exp : 0,  
+            inc : 0, 
+        },
+
+    }
+
+ 
 
     return {
+        addItem :  function(type, des, val) {
+            var newItem, ID;
+            
+            //create new ID
+            if(data.allitems[type].length > 0){
+                ID = data.allitems[type][data.allitems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+            
+            //create new  inc
+            if(type === 'inc'){
+                newItem = new Income(ID, des, val); 
+            }
 
+            if(type === 'exp'){
+                newItem = new Expense(ID, des, val); 
+            }
+              
+            // Push in into our data struct
+            data.allitems[type].push(newItem);
+
+            //return new Item
+            return newItem;
+        },
+
+        testing : function() {
+            console.log(data);
+        }
     };
 
 })();
@@ -13,6 +68,7 @@ var budegetController = (function() {
 //UI CONTROLLER
 var UIController = (function() {
     
+    //List of the DOM elements that will serve as constants
     var DOMStrings = {
         inputType : '.add__type',
         inputDescription : '.add__description',
@@ -43,17 +99,33 @@ var UIController = (function() {
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
-    var DOM = UICtrl.getDOMStrings();
+    var setupEventListeners = function() {
+        
+        var DOM = UICtrl.getDOMStrings();
+        
+        //event listiner
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+        //the key press happens on the global event not just on the click.
+        document.addEventListener('keypress', function(event) {   
+            if(event.keyCode === 13 || event.which === 13){
+                console.log(event);
+                
+                ctrlAddItem();
+            }
+        });
+    }
 
     var ctrlAddItem = function() {
-        
+        var input, newItem;
         console.log("Enter has been pressed.");
 
         //1. Get the field input data from the UI.
-        var input = UICtrl.getInput();
+        input = UICtrl.getInput();
         console.log(input);
 
         //2. Add the item to the budget controller.
+        newItem = budgetCtrl.addItem(input.type, input.desciption, input.value);
 
         //3. Add the item to the UI
 
@@ -64,19 +136,13 @@ var controller = (function(budgetCtrl, UICtrl) {
 
     }
 
-    //event listiner
-    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-
-    //the key press happens on the global event not just on the click.
-    document.addEventListener('keypress', function(event) {
-        
-
-        if(event.keyCode === 13 || event.which === 13){
-            console.log(event);
-            
-            ctrlAddItem();
+    return {
+        init : function() {
+            console.log('Application has started.');
+            setupEventListeners();
         }
-
-    });
+    };
 
 })(budegetController, UIController);
+
+controller.init();
