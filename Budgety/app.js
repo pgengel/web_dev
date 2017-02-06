d003ada78b33d92cb0f540514fc0ee235ebecce4
Pieter Engelbrecht <pieter.engelbrecht@derivco.co.za>
2017-02-06 08:22:14 +0200

    Deleted an item an updated UI.

--- Budgety/app.js
+++ Budgety/app.js
@@ -172,6 +172,12 @@ var UIController = (function() {
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
         },
 
+        deleteListItem : function(selectorID) {
+            var el = document.getElementById(selectorID);
+            el.parentNode.removeChild(el);
+            //document.getElementById(selectorID).parentNode.removeChild()    
+        },
+
         clearFields: function() {
             var fields, fieldsArr;
             fields = document.querySelectorAll(DOMStrings.inputDescription +', ' + DOMStrings.inputValue);
@@ -280,7 +286,10 @@ var controller = (function(budgetCtrl, UICtrl) {
             budgetCtrl.deleteItem(type, ID);
 
             // 2. Delete the item from the the user interface
+            UICtrl.deleteListItem(itemID);
+
             // 3. Update and show the new budget. 
+            updateBudget();
         }
 
     };
