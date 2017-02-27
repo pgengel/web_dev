81a1dfa54f61291a762f5b0546c53801bf4569be
Pieter Engelbrecht <pieter.engelbrecht@derivco.co.za>
2017-02-27 16:04:53 +0200

    The path has been changed.

---
+++ BudgetyEnterprise/src/Budgety.Data/Contexts/BudgetContext.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Budgety.Data.Models;
+
+namespace Budgety.Data.Contexts
+{
+    public sealed class BudgetContext : IDisposable, IBudgetContext
+    {
+        private readonly DataContext _context;
+        private bool _dispose;
+        public BudgetContext()
+        {
+            _context = new DataContext();
+        }
+
+        public DataContext DataContext
+        {
+            get { return _context; }
+        }
+
+
+        public bool CreateBudget(Customer customer)
+        {
+
+            Check.Require(customer.FirstName);
+
+            customer.Id = _context.Customers.Count();
+
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+            return true;
+        }
+
+        public bool DeleteBudget(Customer customer)
+        {
+            _context.Customers.Remove(customer);
+            _context.SaveChanges();
+            return true;
+        }
+
+        public bool UpdateBudget(Customer customer)
+        {
+            Customer entity = _context.Customers.Find(customer.Id);
+
+            if (entity == null)
+            {
+                throw new NotImplementedException("Need to handle this!");
+                return false;
+            }
+
+            _context.Entry(customer).CurrentValues.SetValues(customer);
+            _context.SaveChanges();
+            return true;
+        }
+
+        public ICollection<Customer> GetBudget()
+        {
+            return _context.Customers.OrderBy(p => p.Id).ToArray();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (_dispose || disposing)
+            {
+                return;
+            }
+
+            if (_context != null)
+            {
+                _context.Dispose();
+            }
+
+            _dispose = true;
+        }
+
+        static class Check
+        {
+            public static void Require(string value)
+            {
+                if (value == null)
+                    throw new ArgumentNullException();
+                else if (value.Trim().Length == 0)
+                    throw new ArgumentNullException();
+            }
+        }
+    }
+}
---
+++ BudgetyEnterprise/src/Budgety.Data/Contexts/IBudgetContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Budgety.Data.Models;
+
+namespace Budgety.Data.Contexts
+{
+    public interface IBudgetContext
+    {
+        bool CreateBudget(Customer customer);
+        bool DeleteBudget(Customer customer);
+        bool UpdateBudget(Customer customer);
+        ICollection<Customer> GetBudget();
+    }
+}
