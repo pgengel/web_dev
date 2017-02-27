e914e6ea43f34a05d2f1ed7c9f8b138bab064d90
Pieter Engelbrecht <pieter.engelbrecht@derivco.co.za>
2017-02-27 14:40:49 +0200

    Data project is setup and passing unit test. Database is setup.

---
+++ BudgetyEnterprise/src/Budgety.Data/Contexts/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Budgety.Data.Models;
+
+namespace Budgety.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("Default")
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ BudgetyEnterprise/src/readme.txt
@@ -0,0 +1,3 @@
+Install-Package EntityFramework
+add the proj ref and entity ref in the test proj.
+	EntityFramkework.dll and EntityFramkework.SqlServer.dll from packages.
